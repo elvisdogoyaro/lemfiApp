b74bae9e4baa0da2ed2b137d5eb3042c846d5053
elvisdogoyaro <elvisdogoyaro@gmail.com>
2026-07-25 05:13:08 +0100

    Update urls.js

--- src/components/urls.js
+++ src/components/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://lemfi-api.vercel.app";
 
 export default BASE_URL;
